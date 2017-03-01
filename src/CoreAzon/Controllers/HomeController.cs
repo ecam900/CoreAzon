73702a465e19d7cc7d4c69f0a8d1e52bce1aad42
Eric Crescioni <ecam900@gmail.com>
2017-02-28 23:40:10 -0400

    Add folders: - Model - View - Controller
    Add HomeController, Contact Model and Home/Index view.

---
+++ src/CoreAzon/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CoreAzon.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CoreAzon.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Contact contact = new Models.Contact()
+            {
+                FirstName = "Eric",
+                LastName = "Crescioni"
+            };
+
+            return View(contact);
+        }
+    }
+}
